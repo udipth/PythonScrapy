8c313a6112f8a77ccaf3f740a4afe23cdebd5427
udipth <uskv4ever@gmail.com>
2017-09-01 09:45:40 +0530

    Add files via upload

---
+++ myspider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class myspider(scrapy.Spider):
+    name = "ctrip"
+    start_urls = ['http://hotels.ctrip.com/hotel/taipei617#ctm_ref=hod_hp_sb_lst']
+
+    def parse(self, response):
+        for k in response.css('div.searchresult_list.searchresult_list2'):
+            yield {
+                'name': k.css('searchresult_name, a::attr(title)').re_first(r'\((.*?)\)') ,
+                'rating': k.css('span.hotel_value::text').extract_first() ,
+                'tags': k.css('view_pic view_pic_fall_down, img::attr(src)').extract_first(),
+            }
+
+
